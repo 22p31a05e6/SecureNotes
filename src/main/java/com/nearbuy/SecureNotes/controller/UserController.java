3b962150aa4a3b63033679501bed3241877cd0bc
gopal <22p31a05e6@acet.ac.in>
2026-09-17 17:03:36 +0530

    api created

---
+++ src/main/java/com/nearbuy/SecureNotes/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.nearbuy.SecureNotes.controller;
+
+import com.nearbuy.SecureNotes.entity.User;
+import com.nearbuy.SecureNotes.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        User savedUser = userService.createUser(user);
+
+        return ResponseEntity.ok(savedUser);
+    }
+}
---
+++ src/main/java/com/nearbuy/SecureNotes/service/UserService.java
@@ -0,0 +1,24 @@
+package com.nearbuy.SecureNotes.service;
+
+import com.nearbuy.SecureNotes.entity.User;
+import com.nearbuy.SecureNotes.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        return userRepository.save(user);
+    }
+}
